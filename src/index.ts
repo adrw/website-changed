eb9ed9aa4f5cb279846f04ba0c7a5ce34d38f3d9
Andrew Alexander <account+github@adrw.ch>
2019-04-08 19:10:15 -0400

    Turn off rental tests.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
-export const websites = [
-  // "https://time.is",
-  "http://www.blazeproperties.ca/index.php/properties/abercrombie-place",
-  "http://www.blazeproperties.ca/index.php/properties/belmont-village",
-  "http://www.blazeproperties.ca/index.php/properties/camelot",
-  "http://www.blazeproperties.ca/index.php/properties/cripple-creek",
-  "http://www.blazeproperties.ca/index.php/properties/cripple-creek",
-  "http://www.blazeproperties.ca/index.php/properties/tivoli",
-  "http://www.blazeproperties.ca/index.php/properties/union-lane"
-]
+export const testBlazeVacancies = false
+
+export const websites: string[] = ["https://adrw.xyz"].concat(
+  testBlazeVacancies
+    ? [
+        "http://www.blazeproperties.ca/index.php/properties/abercrombie-place",
+        "http://www.blazeproperties.ca/index.php/properties/belmont-village",
+        "http://www.blazeproperties.ca/index.php/properties/camelot",
+        "http://www.blazeproperties.ca/index.php/properties/cripple-creek",
+        "http://www.blazeproperties.ca/index.php/properties/cripple-creek",
+        "http://www.blazeproperties.ca/index.php/properties/tivoli",
+        "http://www.blazeproperties.ca/index.php/properties/union-lane"
+      ]
+    : []
+)
--- tests/websites.test.ts
+++ tests/websites.test.ts
@@ -1,47 +1,49 @@
-import { websites } from "../src"
+import { testBlazeVacancies, websites } from "../src"
 import axios from "axios"
 const htmlParser = require("html-parser")
 
-describe("blaze-vacancies", () => {
-  it("blaze-vacancies", () => {
-    return axios
-      .get("http://www.blazeproperties.ca/index.php/vacancies")
-      .then(resp => {
-        expect(resp.data).toBeDefined()
-        let vacanciesH2 = false
-        let vacanciesList = false
-        let vacanciesSnapshot: string[] = []
-        htmlParser.parse(resp.data, {
-          openElement: (name: string) => {
-            if (name == "h2") {
-              vacanciesH2 = true
+if (testBlazeVacancies) {
+  describe("blaze-vacancies", () => {
+    it("blaze-vacancies", () => {
+      return axios
+        .get("http://www.blazeproperties.ca/index.php/vacancies")
+        .then(resp => {
+          expect(resp.data).toBeDefined()
+          let vacanciesH2 = false
+          let vacanciesList = false
+          let vacanciesSnapshot: string[] = []
+          htmlParser.parse(resp.data, {
+            openElement: (name: string) => {
+              if (name == "h2") {
+                vacanciesH2 = true
+              }
+            },
+            closeElement: (name: string) => {
+              if (vacanciesH2 && name === "h2") {
+                vacanciesList = true
+              }
+            },
+            text: (value: string) => {
+              if (vacanciesH2 && value !== "Vacancies") {
+                vacanciesH2 = false
+              }
+              if (vacanciesList && value === "Sorry, no vacancy.") {
+                vacanciesList = false
+              }
+              if (
+                vacanciesList &&
+                value.trim().length !== 0 &&
+                value.trim() !== ","
+              ) {
+                vacanciesSnapshot.push(value)
+              }
             }
-          },
-          closeElement: (name: string) => {
-            if (vacanciesH2 && name === "h2") {
-              vacanciesList = true
-            }
-          },
-          text: (value: string) => {
-            if (vacanciesH2 && value !== "Vacancies") {
-              vacanciesH2 = false
-            }
-            if (vacanciesList && value === "Sorry, no vacancy.") {
-              vacanciesList = false
-            }
-            if (
-              vacanciesList &&
-              value.trim().length !== 0 &&
-              value.trim() !== ","
-            ) {
-              vacanciesSnapshot.push(value)
-            }
-          }
+          })
+          expect(vacanciesSnapshot).toMatchSnapshot()
         })
-        expect(vacanciesSnapshot).toMatchSnapshot()
-      })
+    })
   })
-})
+}
 
 websites.map(url => {
   describe("websites", () => {
